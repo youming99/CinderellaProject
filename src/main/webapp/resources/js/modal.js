38d6f7fda5b0f7c398fa55e72d99708b4f1060f0
황유미 <55625444+youming99@users.noreply.github.com>
2021-01-21 13:51:38 +0900

    Add files via upload

---
+++ src/main/webapp/resources/js/modal.js
@@ -0,0 +1,14 @@
+/*!
+  * Bootstrap v4.4.1 (https://getbootstrap.com/)
+  * Copyright 2011-2019 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+  */
+
+jQuery(document).ready(function() {
+	$("#modal").show();
+});
+function closeModal() {
+	$('.searchModal').hide();
+};
+
+
